4995b38cd6d589054418be1e01ec42326c64d81f
alexander16108 <alexandermayowa05@gmail.com>
2022-01-10 17:21:40 +0100

    fixing linter errors

--- school-library/person/persons.rb
+++ school-library/person/persons.rb
@@ -1,26 +1,22 @@
 class Person
-  def initialize(name = "unknown", age, parent_permission = true)
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name = 'unknown', _parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
   end
 
-  attr_reader :id
+  def of_age?
+    return true if @age >= 18
 
-  attr_accessor :name
-  attr_accessor :age
-
-  def is_of_age?
-    if @age <= 18
-      return true
-    else
-      return false
+    false
   end
 
   def can_use_services?
-    if @age <= 18 or parent_permission = true
-      return true
-    else
-      return false
+    return true if @age >= 18 || parent_permission
+
+    false
   end
 end
