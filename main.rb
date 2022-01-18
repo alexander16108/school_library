0fa23fd71571397d86f9c69f48bf97b021bf4c04
alexander16108 <alexandermayowa05@gmail.com>
2022-01-18 07:57:41 +0100

    refactoring code

--- main.rb
+++ main.rb
@@ -7,8 +7,6 @@ require './school-library/book'
 class Methods
   def initialize
     @person_array = []
-    @books = []
-    @rental_array = []
   end
 
   def create_person
@@ -63,66 +61,9 @@ class Methods
     end
   end
 
-  def create_book
-    print 'Title: '
-    book_title = gets.chomp
-
-    print 'Author: '
-    book_author = gets.chomp
-
-    puts 'Book created successfully!'
-
-    book = Book.new(book_title, book_author)
-    @books.push({
-                  output: "Title: #{book.title}, Author: #{book.author}",
-                  object: book
-                })
-  end
-
-  def book_list
-    @books.each do |book|
-      puts book[:output]
-    end
-  end
-
   def people_list
     @person_array.each do |person|
       puts person[:output]
-    end
-  end
-
-  def create_rental
-    puts 'Select a book from the following list by number: '
-    @books.each_with_index do |book, index|
-      puts "#{index}) #{book[:output]}"
-    end
-    book_selected = Integer(gets.chomp)
-    book_chosen = @books[book_selected][:object]
-
-    puts 'Select a person from the following list by number (not id): '
-    @person_array.each_with_index do |person, index|
-      puts "#{index}) #{person[:output]}"
-    end
-    person_selected = Integer(gets.chomp)
-    person_chosen = @person_array[person_selected][:object]
-
-    print 'Date: '
-    rental_date = gets.chomp
-
-    @rental_array.push(Rental.new(rental_date, book_chosen, person_chosen))
-  end
-
-  def rental_list
-    print 'ID of person: '
-    person_id = Integer(gets.chomp)
-    puts 'Rental: '
-
-    @rental_array.each do |rental|
-      if person_id == rental.person.id
-        puts "Date: #{rental.date}, Book: \"#{rental.book.title}\" by #{rental.book.author}"
-      else
-        puts 'ID does not exist'
-      end
     end
   end
 end
@@ -151,21 +92,23 @@ class App
   end
 
   method = Methods.new
+  book = BooksList.new
+  rent = RentalsBook.new
 
   loop do
     case home_page
     when 1
-      method.book_list
+      book.book_list
     when 2
       method.people_list
     when 3
       method.create_person
     when 4
-      method.create_book
+      book.create_book
     when 5
-      method.create_rental
+      rent.create_rental
     when 6
-      method.rental_list
+      rent.rental_list
     when 7
       puts 'Thank you for using the app!'
       exit
